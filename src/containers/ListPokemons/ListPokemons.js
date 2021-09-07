69debd305042540a15ed158307b00ce7c357485d
4str0n4ut <sp4ce.4str0n4ut@gmail.com>
2021-09-07 10:19:10 +0300

    refactor: recode containers using mapStateToProps

--- src/containers/ListPokemons/ListPokemons.js
+++ src/containers/ListPokemons/ListPokemons.js
@@ -1,59 +1,61 @@
 import React, { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { getPokemons, loadPage } from '../../store/actions/actions';
 import PokemonList from '../../components/Pokemon-list/pokemon-list';
-import { POKEMONS_PER_PAGE } from '../../store/constants/constants'; 
+import { POKEMONS_PER_PAGE } from '../../store/constants/constants';
 
-const ListPokemons = () => {
-  
-  const dispatch = useDispatch();
-  const pokemons = useSelector(state => state.pokemons.pokemons);
-  const page = useSelector(state => state.pokemons.page);
-  const isLoading = useSelector(state => state.pokemons.isLoading);
-  const errorMsg = useSelector(state => state.errorMsg);
+const ListPokemons = ({getPokemons, loadPage, pokemons, page, isLoading, errorMsg}) => {
 
-  useEffect(() => {
+    useEffect(() => {
     const fetchPokemons = async (page) => {
-      await dispatch(getPokemons(page));
+      await getPokemons(page);
     }
 
     if (pokemons.length / POKEMONS_PER_PAGE === page) {
       fetchPokemons(page);
     }
     //eslint-disable-next-line
-  }, [page, dispatch]);
-
+  }, [page, getPokemons]);
+  
   const onClickHandler = useCallback((e) => {
     e.preventDefault();
-    dispatch(loadPage());
-  }, [dispatch]);
+    loadPage();
+  }, [loadPage]);
 
   const showPokemons = () => {
+    if (errorMsg !== "") {
+      return <h1>Error: {errorMsg}</h1>;
+    }
+
     if (pokemons) {
       return (
         <div>
-            <PokemonList pokemons={pokemons} />
-            <div className='load-more'>
-              <button onClick={onClickHandler} className='load-more-button'>
-                {isLoading ? 'Loading...' : 'Load more'}
-              </button>
-            </div>
+          <PokemonList pokemons={pokemons} />
+          <div className="load-more">
+            <button onClick={onClickHandler} className="load-more-button">
+              {isLoading ? "Loading..." : "Load more"}
+            </button>
+          </div>
         </div>
       );
     }
 
-		if(errorMsg) {
-			return <h1>Error: {errorMsg}</h1>;
-		}
+    return <h1>Unable to get data</h1>;
+  };
 
-		return <h1>Unable to get data</h1>;
-  }
-
-  return (
-    <>
-      {showPokemons()}
-    </>
-  );
+  return <>{showPokemons()}</>;
 }
 
-export default ListPokemons;
+const mapStateToProps = state => ({
+  pokemons: state.pokemons.pokemons,
+  page: state.pokemons.page,
+  isLoading: state.pokemons.isLoading,
+  errorMsg: state.pokemons.errorMsg,
+});
+
+const mapDispatchToProps = {
+  getPokemons,
+  loadPage,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListPokemons);
--- src/containers/ShowProfile/ShowProfile.js
+++ src/containers/ShowProfile/ShowProfile.js
@@ -1,52 +1,50 @@
 import React, {useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { getPokemon } from '../../store/actions/actions';
 import PokemonProfile from '../../components/Pokemon-profile/pokemon-profile';
 import Loader from '../../components/Loader/loader';
 
-const ShowProfile = props => {
-    const {id} = props.match.params;
-    const dispatch = useDispatch();
-    const pokemon = useSelector(state => state.pokemon.pokemon);
-    const pokemons = useSelector(state => state.pokemons.pokemons);
-    const isLoading = useSelector(state => state.pokemon.isLoading);
-    const errorMsg = useSelector(state => state.pokemon.errorMsg);
+const ShowProfile = ({ pokemon, pokemons, isLoading, errorMsg, getPokemon, match }) => {
+  const {id} = match.params;
 
-    useEffect(() => {
-        dispatch(getPokemon(id));
-    }, [id, dispatch]);
+  useEffect(() => {
+      getPokemon(id);
+  }, [getPokemon, id]);
 
-    try {
-        if (pokemons.find(pokemon => pokemon.id === Number(id)).isCatched) {
-            pokemon.isCatched = true;
-            pokemon.timestamp = pokemons.find(pokemon => pokemon.id === Number(id)).timestamp;
-        }
-    } catch {
-        pokemon.isCatched = false;
+  try {
+    if (pokemons.find((pokemon) => pokemon.id === Number(id)).isCatched) {
+      pokemon.isCatched = true;
+      pokemon.timestamp = pokemons.find((pokemon) => pokemon.id === Number(id)).timestamp;
     }
+  } catch {
+    pokemon.isCatched = false;
+  }
 
     const showPokemon = () => {
-        if(isLoading)
-			return (
-        <Loader />
-      );
+      if (isLoading) return <Loader />;
 
-        if(errorMsg !== '')
-			return <h1>Error: {errorMsg}</h1>
+      if (errorMsg !== "") return <h1>Error: {errorMsg}</h1>;
 
-        if (pokemon)
+      if (pokemon)
         return (
-            <div>
-                <PokemonProfile pokemon={pokemon}/>
-            </div>        
+          <div>
+            <PokemonProfile pokemon={pokemon} />
+          </div>
         );
-    }
+    };
 
-    return (
-        <>
-            {showPokemon()}
-        </>
-    );
+    return <>{showPokemon()}</>;
 }
 
-export default ShowProfile;
+const mapStateToProps = state => ({
+    pokemon: state.pokemon.pokemon,
+    pokemons: state.pokemons.pokemons,
+    isLoading: state.pokemon.isLoading,
+    errorMsg: state.pokemon.errorMsg,
+  });
+
+const mapDispatchToProps = {
+    getPokemon,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowProfile);
